00cc8dcdd3ba5cce8cfcb2f88ab6061b5a7b1fa7
Daniel <daniel.meppiel@sonarsource.com>
2022-01-24 11:10:53 +0100

    Fix the vulnerability

--- myserver.js
+++ myserver.js
@@ -1,3 +1,4 @@
+import sanitizeHtml from "sanitize-html";
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
@@ -9,14 +10,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/api/hello", (req, res) => {
   console.log("Hello called");
   res.send({ express: "Hello From Express" });
-  for (let i = 0; i < 10; i--) {
-    console.log(i);
-  }
 });
 
 app.post("/api/world", (req, res) => {
   console.log(req.body);
-  res.send("You sent:" + req.body.post);
+  res.send("You sent:" + sanitizeHtml(req.body.post));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
